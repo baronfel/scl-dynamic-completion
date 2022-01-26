64723a140e62bbb0f469164dee611946cc753aca
Chet Husk <chusk3@gmail.com>
2022-01-26 17:10:18 -0600

    minimize code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Completions;
 using System.CommandLine.Parsing;
 using System.CommandLine.Builder;
