86b29b5597dbc61e9ec5ed6fc011123b81c7325a
Chet Husk <chusk3@gmail.com>
2022-01-26 16:10:06 -0600

    sample

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.CommandLine;
+using System.CommandLine.Completions;
+using System.CommandLine.Parsing;
+using System.CommandLine.Builder;
+
+new CommandLineBuilder(new DateCommand()).UseParseDirective().UseParseErrorReporting().UseExceptionHandler().UseHelp().UseSuggestDirective().Build().InvokeAsync(args);
+
+class DateCommand : Command
+{
+    private Argument<string> subject = new Argument<string>("subject", "The subject of the appointment");
+    private Option<DateTime> date = new Option<DateTime>("--date", "The day of week to schedule. Should be within two weeks");
+    public DateCommand() : base("schedule", "makes an appointment for sometime in the next week")
+    {
+        this.AddArgument(subject);
+        this.AddOption(date);
+
+        this.SetHandler((string message, DateTime date) => {
+            Console.WriteLine($"Scheduled \"{message}\" for {date}");
+        }, subject, date);
+    }
+
+    public override IEnumerable<CompletionItem> GetCompletions(CompletionContext context)
+    {   
+        var today = System.DateTime.Today;
+        foreach (var i in Enumerable.Range(1, 14)) {
+            yield return new CompletionItem(today.AddDays(i).ToShortDateString());
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,28 @@
+# Dynamic System.CommandLine completion sample
+
+This sample illustrates the use of `GetCompletions` to generate dynamic
+completions for a command.  This command wants to suggest dates in the future,
+but cannot know the current date at compile-time, only at runtime.
+
+To test this:
+
+* run the app with `dotnet run [suggest] "some appointment" --date ""`
+* you should see the next two weeks of dates printed to the command line:
+
+```shell
+PS C:\Users\chethusk\oss\Scratch\scl-dynamic-completion> dotnet run [suggest] "blah blah" --date "1"
+1/27/2022
+1/28/2022
+1/29/2022
+1/30/2022
+1/31/2022
+2/1/2022
+2/2/2022
+2/3/2022
+2/4/2022
+2/5/2022
+2/6/2022
+2/7/2022
+2/8/2022
+2/9/2022
+```
