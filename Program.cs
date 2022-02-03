fcf5583573826450339b67fed66f6a3e999d7e12
Chet Husk <chusk3@gmail.com>
2022-02-03 12:49:35 -0600

    example for sort order + docs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,11 @@ class DateCommand : Command
             var today = System.DateTime.Today;
             var dates = new List<CompletionItem>();
             foreach (var i in Enumerable.Range(1, 14)) {
-                 dates.Add (new CompletionItem(today.AddDays(i).ToShortDateString()));
+                 dates.Add (new CompletionItem(
+                        today.AddDays(i).ToShortDateString(),  // actual value
+                        sortText: $"{i:2}" // convert the integer to 2-digits in length, so that sorting is based on 01, 02, ..., 14.
+                                           // without this, the dates are sorted based on the Label, which is incorrect
+                ));
             }
             return dates;
         }
