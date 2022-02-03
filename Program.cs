f631d856cce701f386d54e204e59b711af1204ea
Chet Husk <chusk3@gmail.com>
2022-02-03 11:48:39 -0600

    completions only on the date parameters

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Completions;
 using System.CommandLine.Parsing;
-using System.CommandLine.Builder;
 
-new CommandLineBuilder(new DateCommand()).UseParseDirective().UseParseErrorReporting().UseExceptionHandler().UseHelp().RegisterWithDotnetSuggest().UseSuggestDirective().Build().InvokeAsync(args);
+await new DateCommand().InvokeAsync(args);
 
 class DateCommand : Command
 {
@@ -14,18 +12,18 @@ class DateCommand : Command
     {
         this.AddArgument(subject);
         this.AddOption(date);
+        date.AddCompletions((ctx) => {
+            var today = System.DateTime.Today;
+            var dates = new List<CompletionItem>();
+            foreach (var i in Enumerable.Range(1, 14)) {
+                 dates.Add (new CompletionItem(today.AddDays(i).ToShortDateString()));
+            }
+            return dates;
+        }
+        );
 
         this.SetHandler((string message, DateTime date) => {
             Console.WriteLine($"Scheduled \"{message}\" for {date}");
         }, subject, date);
     }
-
-    public override IEnumerable<CompletionItem> GetCompletions(CompletionContext context)
-    {   
-        var today = System.DateTime.Today;
-        foreach (var i in Enumerable.Range(1, 14)) {
-            yield return new CompletionItem(today.AddDays(i).ToShortDateString());
-        }
-    }
 }
-
